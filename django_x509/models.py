4fbba2cd5896b8cce71de60f048d86635a624586
Federico Capoano <nemesis@ninux.org>
2017-01-31 15:22:02 +0100

    Revert "[models] Removed reduntant "abstract = False" statements"
    This reverts commit 429575f8fd7b3fbc12d7c22174f608c071969ecd.

--- django_x509/models.py
+++ django_x509/models.py
@@ -5,9 +5,13 @@ class Ca(AbstractCa):
     """
     Concrete Ca model
     """
+    class Meta(AbstractCa.Meta):
+        abstract = False
 
 
 class Cert(AbstractCert):
     """
     Concrete Cert model
     """
+    class Meta(AbstractCert.Meta):
+        abstract = False
