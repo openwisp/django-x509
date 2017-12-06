6874dd519d4912097c8ed7817c4855108f9ffea5
Federico Capoano <nemesis@ninux.org>
2017-12-06 14:48:03 +0100

    Revert "[tests] Fixed test_fill_subject_None_attrs #20"
    This reverts commit 15dbbed077e7163c1bc60ba1651b0a9d67eef09d.

--- django_x509/tests/test_cert.py
+++ django_x509/tests/test_cert.py
@@ -335,17 +335,17 @@ k9Y1S1C9VB0YsDZTeZUggJNSDN4YrKjIevYZQQIhAOWec6vngM/PlI1adrFndd3d
         # ensure no exception raised if model attrs are set to None
         x509 = crypto.X509()
         cert = Cert(name='test', ca=self._create_ca())
-        cert._fill_subject(x509)
+        cert._fill_subject(x509.get_subject())
         self.country_code = 'IT'
-        cert._fill_subject(x509)
+        cert._fill_subject(x509.get_subject())
         self.state = 'RM'
-        cert._fill_subject(x509)
+        cert._fill_subject(x509.get_subject())
         self.city = 'Rome'
-        cert._fill_subject(x509)
+        cert._fill_subject(x509.get_subject())
         self.organization_name = 'OpenWISP'
-        cert._fill_subject(x509)
+        cert._fill_subject(x509.get_subject())
         self.email = 'test@test.com'
-        cert._fill_subject(x509)
+        cert._fill_subject(x509.get_subject())
 
     def test_cert_create(self):
         ca = Ca(name='Test CA')
