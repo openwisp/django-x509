275134d94d9182dc3fb1ae5cd66ade571475acc2
Anna Sidwell <anna@takkaria.org>
2019-11-01 20:08:27 +0100

    [upgrade] Replace use of admin_static, deprecated in Django 2.1

--- django_x509/base/admin.py
+++ django_x509/base/admin.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.conf.urls import url
 from django.contrib.admin import ModelAdmin
-from django.contrib.admin.templatetags.admin_static import static
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.templatetags.static import static
 from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
