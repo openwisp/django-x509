3a11f10e28babc21271f9ec07f20232bab18cd88
ManishShah120 <mkshah141@gmail.com>
2021-05-31 15:56:46 +0530

    [mixin] Moved download `crl-view` to mixin #116
    Fixes #116

--- django_x509/tests/test_ca.py
+++ django_x509/tests/test_ca.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.urls import reverse
@@ -13,12 +14,19 @@ from . import TestX509Mixin
 
 Ca = load_model('django_x509', 'Ca')
 Cert = load_model('django_x509', 'Cert')
+User = get_user_model()
 
 
 class TestCa(TestX509Mixin, TestCase):
     """
     tests for Ca model
     """
+
+    def setUp(self):
+        User.objects.create_superuser(
+            username='admin', password='tester', email='admin@admin.com'
+        )
+        self.client.login(username='admin', password='tester')
 
     def _prepare_revoked(self):
         ca = self._create_ca()
