7618193c12141e1f5f6a0d5d8d858adc24ab9c47
Gagan Deep <pandafy.dev@gmail.com>
2023-06-20 02:16:32 +0530

    [deps] Added support for Django 4.1.0 and 4.2.0
    - Dropped support for Python 3.7
    - Dropped support for Django 4.0
    - Bumped cryptography~=41.0.1
    - Bumped pyopenssl

--- tests/openwisp2/sample_x509/apps.py
+++ tests/openwisp2/sample_x509/apps.py
@@ -2,5 +2,8 @@ from django_x509.apps import DjangoX509Config
 
 
 class SampleX509Config(DjangoX509Config):
-    name = 'sample_x509'
+    name = 'openwisp2.sample_x509'
     verbose_name = 'sample_x509'
+
+
+del DjangoX509Config
