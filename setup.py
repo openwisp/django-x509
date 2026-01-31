4ae2b2dcdac4b22097e3eb91afd3586978a08f4f
SAMurai-16 <goforsamyak.c@gmail.com>
2026-01-31 09:57:51 +0000

    [change] Replaced jsonfield.JSONField with models.JSONField #181 Migrated from the third-party jsonfield package to Django's built-in models.JSONField in sample app.
    Fixes #181

--- setup.py
+++ setup.py
@@ -3,26 +3,6 @@ from setuptools import find_packages, setup
 
 from django_x509 import get_version
 
-
-def get_install_requires():
-    """
-    parse requirements.txt, ignore links, exclude comments
-    """
-    requirements = []
-    for line in open("requirements.txt").readlines():
-        # skip to next iteration if comment or empty line
-        if (
-            line.startswith("#")
-            or line == ""
-            or line.startswith("http")
-            or line.startswith("git")
-        ):
-            continue
-        # add line to requirements
-        requirements.append(line)
-    return requirements
-
-
 setup(
     name="django-x509",
     version=get_version(),
@@ -38,7 +18,9 @@ setup(
     packages=find_packages(exclude=["tests", "docs"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=get_install_requires(),
+    install_requires=[
+        "cryptography~=46.0.3",
+    ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
